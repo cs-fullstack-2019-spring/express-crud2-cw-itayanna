31dd85a57eb88c61282e06130c1491fc1ac40d39
itayanna <icage97@gmail.com>
2019-03-26 16:15:28 -0500

    classwork

--- app.js
+++ app.js
@@ -6,14 +6,14 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-// var fillPostData = require('./routes/fillPostData');
+//var fillPostData = require('./routes/fillPostData');
 
 var app = express();
 
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = '';
+var mongoDB = 'mongodb+srv://admin:admin@cluster0-taxud.mongodb.net/test?retryWrites=true\n';
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
@@ -31,7 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-// app.use('/fillData', fillPostData);
+//app.use('/fillData', fillPostData);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,51 @@
 var express = require('express');
 var router = express.Router();
+var PostCollection = require('../models/PostSchema');
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  PostCollection.find({}, (errors, results)=>
+  {
+    if (errors) res.send(errors);
+    else {
+      context = {
+        title: "Post Collection",
+        allPost: results,
+      };
+      res.render('index', context);
+    }
+  });
 });
+
+
+router.get('/create', (req, res) => res.render('createAlbum'));
+
+router.get('/saveAlbum', (req, res)=>{
+  PostCollection.create(
+      {userId: req.query.userId,
+        id: req.query.id,
+        title: req.query.title,
+        body: req.query.body}, (errors)=>{
+        if (errors) res.send(errors);
+        else res.redirect("/");
+      })
+});
+
+router.get('/update/:id/:title/:body', (req,res)=>{
+  PostCollection.updateOne(
+      {id: req.params.id},
+      {
+        title: req.params.title,
+          body: req.params.body}, (errors, results)=>{
+        if (errors) res.send(errors);
+        else res.send(results);
+      })
+} );
+
+
+
 
 module.exports = router;
